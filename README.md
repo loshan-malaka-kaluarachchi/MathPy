ecd021d66794402cd43bb82355a981c2d3330da8
Loshan Malaka Kaluarachch <kloshan13@gmail.com>
2024-04-18 22:40:02 +0530

    Added some algebraic writing conventions

--- README.md
+++ README.md
@@ -12,7 +12,20 @@ $$ a \cdot x^{p} + b \cdot x^{p} = \( a + b\) \cdot x^{p} $$
 ### Case 2: If coefficients and powers equal to one, they can be hidden
 $$ 1 \cdot x^{1} = x $$
 
+### Case 3: If coefficient is zero
+$$ a = 0; p = ? -> 0 $$
 
+### Case 4: If the index is equal to zero 
+$$ a = ?; p = 0 -> a $$
+
+### Case 5: If the coefficient and index is equal to 1
+$$ a = 1; p = 1 -> x $$
+
+### Case 6: If the index is equal to 1
+$$ a = ?; p = 1 -> ax $$
+
+### Case 7: If both the coefficient and index are neither zero nor one
+$$ a = ?; p = ? -> ax^p $$
 
 
 
--- classes/symbolic.py
+++ classes/symbolic.py
@@ -1,9 +1,32 @@
 class Math_Symb:
     
+    @staticmethod
+    def check_attr(self_var:int,value:int) -> bool:
+        if self_var == value:
+            return True
+        else:
+            return False
+    
     def __init__(self,coefficient:float, symbol:str, index:float) -> None:
         self.coefficient = coefficient
         self.symbol = symbol
         self.index = index
     
     def __add__(self, other):
-        return self.coefficient + other.coefficient
+        if self.symbol == other.symbol and self.index == other.index:
+            return self.coefficient + other.coefficient
+    
+    def __repr__(self) -> str:
+        if self.coefficient == 0:
+            return ''
+        elif self.index == 0:
+            return f'{self.coefficient}'
+        elif self.coefficient == 1 and self.index == 1:
+            return f'{self.symbol}'
+        elif self.index == 1:
+            return f'{self.coefficient}{self.symbol}'
+        else:
+            return f'{self.coefficient}{self.symbol}^{self.index}'
+            
+        
+        
